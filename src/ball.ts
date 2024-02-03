0b6753f151e973ea2b18f978adb9cf98f480091e
abu-hiba <tbolton96@icloud.com>
2024-02-03 23:07:25 +0000

    Adds logic to move balls and detect wall collisions

--- src/ball.ts
+++ src/ball.ts
@@ -7,24 +7,55 @@ type Position = Vector;
 type Velocity = Vector;
 
 export class Ball {
+    private ctx: CanvasRenderingContext2D;
+
     constructor(
+        private canvas: HTMLCanvasElement,
         private position: Position,
         private radius: number,
         private velocity: Velocity = { x: 0, y: 0 },
         private colour: string = 'rgba(29, 0, 255, 0.5)',
         private mass: number = Math.PI * radius ** 2,
-    ) {};
+    ) {
+        const context = this.canvas.getContext('2d');
+        if (!context) throw new Error('Could not get canvas context');
+        this.ctx = context;
+    };
 
-    draw(ctx: CanvasRenderingContext2D) {
-        ctx.save();
+    draw() {
+        this.ctx.save();
         const path = new Path2D();
-        ctx.beginPath();
-        ctx.fillStyle = this.colour;
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.colour;
         path.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
-        ctx.fill(path);
-        ctx.restore();
+        this.ctx.fill(path);
+        this.ctx.restore();
 
         return path;
+    };
+
+    move() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    };
+
+    detectCollision() {
+        const collidesWithRightEdge =
+            this.position.x + this.velocity.x > this.canvas.width - this.radius;
+        const collidesWithLeftEdge =
+            this.position.x + this.velocity.x < this.radius;
+        const collidesWithBottomEdge =
+            this.position.y + this.velocity.y > this.canvas.height - this.radius;
+        const collidesWithTopEdge =
+            this.position.y + this.velocity.y < this.radius;
+
+        if (collidesWithRightEdge || collidesWithLeftEdge) {
+            this.velocity.x = -this.velocity.x;
+        }
+
+        if (collidesWithBottomEdge || collidesWithTopEdge) {
+            this.velocity.y = -this.velocity.y;
+        }
     };
 };
 
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,71 @@ import './style.css'
 
 const canvas = document.querySelector<HTMLCanvasElement>('#canvas');
 if (!canvas) {
-    throw new Error("Canvas element not available"); 
+    throw new Error('Canvas element not available');
 }
 
 const context = canvas.getContext('2d');
 if (!context) {
-    throw new Error("Cannot get canvas context");
+    throw new Error('Cannot get canvas context');
 }
 
-const ball1 = new Ball(
-    { x: 100, y: 100 },
-    10,
-);
+const getRand = (min: number, max: number): number => {
+    return Math.random() * (max - min) + min;
+};
 
-ball1.draw(context);
+const createRandomBall = () => {
+    const radius = getRand(10, 40);
+
+    const initialPosition = {
+        x: getRand(radius, canvas.width),
+        y: getRand(radius, canvas.height),
+    };
+
+    const velocity = {
+        x: getRand(1, 10),
+        y: getRand(1, 10),
+    };
+
+    return new Ball(
+        canvas,
+        initialPosition,
+        radius,
+        velocity,
+    );
+};
+
+const balls = [createRandomBall()];
+
+const loop = () => {
+    requestAnimationFrame(loop);
+
+    context.fillStyle = 'white';
+    context.beginPath();
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    balls.forEach((ball) => {
+        ball.draw();
+        ball.detectCollision();
+        ball.move();
+    });
+};
+
+loop();
+
+const addButton = document.createElement('button');
+addButton.textContent = 'Add ball';
+addButton.addEventListener('click', () => {
+    balls.push(createRandomBall());
+});
+
+const removeButton = document.createElement('button');
+removeButton.textContent = 'Remove ball';
+removeButton.addEventListener('click', () => {
+    balls.pop();
+});
+
+const root = document.querySelector('#app');
+if (!root) throw new Error('App root not found');
+root.appendChild(addButton);
+root.appendChild(removeButton);
 
